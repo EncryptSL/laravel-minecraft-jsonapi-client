c7703bd83d317d45df4ffc73e5fb4294748da783
Aurélien SCHILTZ <aure.lienschiltz@gmail.com>
2019-09-07 09:26:19 +0200

    fix: Provider Has Not the Good Name
    Which leads into Provider not found
    Fixes #1

--- src/Providers/MinecraftJsonApiServiceProvider.php
+++ src/Providers/MinecraftJsonApiServiceProvider.php
@@ -6,14 +6,14 @@ use Illuminate\Support\ServiceProvider;
 use Kangoo13\Minecraft\JsonApi\Services\MinecraftJsonApiService;
 
 /**
- * Class MinecraftJsonApiProvider
+ * Class MinecraftJsonApiServiceProvider
  *
  * @package   Kangoo13\Minecraft\JsonApi\Providers
  * @author    Aurélien SCHILTZ <aurelienschiltz@gmail.com>
  * @copyright 2016-2019 Aurélien SCHILTZ
  * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
  */
-class MinecraftJsonApiProvider extends ServiceProvider
+class MinecraftJsonApiServiceProvider extends ServiceProvider
 {
     public function boot()
     {
